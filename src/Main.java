55bbfe3185c25174e7065e145dc3432747d04ffc
AlexDstart <demyanalex@gmail.com>
2022-11-21 22:20:02 +0300

    задача 2

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,23 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Task 1");
         int clientOS = 0;
-        if(clientOS==1) {
-            System.out.println(" Установите версию приложения для Android по ссылке ");
-        }else{
-            System.out.println(" Установите версию приложения для IOS по ссылке ");
+        int clientDeviceYear = 2016;
+        if (clientOS == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println(" Установите облегченную версию Android по ссылке ");
+
+            } else {
+                System.out.println(" Установите обычную версию приложения для Android по ссылке ");
+            }
+        }
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println(" Установите облегченную версию IOS по ссылке ");
+
+            } else {
+                System.out.println(" Установите обычную версию приложения для IOS по ссылке ");
+            }
         }
     }
-    }
+}
+
